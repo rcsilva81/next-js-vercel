16c7f505a80b2a1420be8d289805666968955060
Robert Silva <robert.c.silva@gmail.com>
2020-11-22 23:57:21 -0300

    segunda página

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
 import { useState } from 'react';
 
 function Home(){
-    return{
-        <div>
-            <h1>Home 2</h1>
-            <Contador />
-            <h2>Home 3</h2>
-        </div>
-    } 
-}
-
-
-function Contador(){
-    const [contador, setContador]=useState(1);
-    
-    function adcionarContador(){
-        setContador(contador+1);
-    }
-
-    return {
-        <div> 
-            <div>{contador}</div>
-            <button onClick={adcionarContador}>Adicionar</button>
-        </div>
-    }
+    return <h1>Home 2</h1>;
 }
 
 export default Home;
